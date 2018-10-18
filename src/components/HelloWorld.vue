1bca2d9e2a4b6094e1e12fb9be870f1e3b80127a
Flogstad <peter.flogstad@gmail.com>
2018-10-18 16:13:12 +0200

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+      <button v-on:click="fetchStations()">Hent Stasjoner</button>
+      <div v-for="station in stations">
+          <p>{{station.title}}</p>
+      </div>
+  </div>
+</template>
+
+<script>
+    import axios from 'axios';
+    import Api from '../Api';
+
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+    data() {
+        return {
+            stations: []
+        }
+    },
+    methods: {
+      fetchStations() {
+          axios.get(Api.getStasjoner())
+              .then(res => {
+                  this.stations = res.data;
+              });
+      }
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ vue.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    outputDir: '../resources/public',
+    devServer: {
+        // proxy: "http://localhost:8080",   Legg til hvis man ønsker at kall skal proxies til 8080
+        port: 3000
+    }
+};

0d92066e5d5d8b1bb55e6d4169ea7693184da5da
Flogstad <peter.flogstad@gmail.com>
2018-10-18 16:47:17 +0200

    Endret circleCi config

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,1 @@
-module.exports = {
-    outputDir: '../resources/public',
-    devServer: {
-        // proxy: "http://localhost:8080",   Legg til hvis man ønsker at kall skal proxies til 8080
-        port: 3000
-    }
-};
+module.exports = {};

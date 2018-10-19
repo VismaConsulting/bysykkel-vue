e8d936ee57ac8930b7af42b24740ea05f9d90770
Flogstad <peter.flogstad@gmail.com>
2018-10-19 13:42:56 +0200

    Lagt til kube endpoints og fjernet cd

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ var path = require('path');
 
 app.use('/', express.static(path.join(__dirname, 'dist')));
 
+app.get('/actuator/isAlive', (err, res) => {
+    res.status(200).send("Is Alive!")
+});
+
+app.get('/actuator/isReady', (err, res) => {
+    res.status(200).send("Is Ready!")
+});
+
 //var port = process.env.PORT || 80     HEROKU
 var port = process.env.PORT || 8080
 app.listen(port)

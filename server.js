8c1f7f1f860ed3c20bfc227e7dbf2e1ff33f7ffd
Flogstad <peter.flogstad@gmail.com>
2018-10-19 11:09:30 +0200

    Oppdatert dockerfile

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var path = require('path');
 
 app.use('/', express.static(path.join(__dirname, 'dist')));
 
-var port = process.env.PORT || 80
+//var port = process.env.PORT || 80     HEROKU
+var port = process.env.PORT || 8080
 app.listen(port)
 console.log('starting project at ' + port)
